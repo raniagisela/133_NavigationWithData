66c5db924f27d2a84cfbe37288d21b7f1d982a3c
raniagisela <raniagslp@gmail.com>
2023-11-16 00:40:13 +0700

    add text to formatUI

--- app/src/main/java/com/example/esjumbo/ui/theme/komponen/FormatUI.kt
+++ app/src/main/java/com/example/esjumbo/ui/theme/komponen/FormatUI.kt
@@ -1,10 +1,14 @@
 package com.example.esjumbo.ui.theme.komponen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.esjumbo.R
 
 @Composable
@@ -14,4 +18,32 @@ fun FormatLabelHarga(subtotal: String, modifier: Modifier = Modifier) {
         modifier = modifier,
         style = MaterialTheme.typography.headlineSmall
     )
+}
+
+@Composable
+fun DataPemesan(
+    namaPemesan: String,
+    alamatPemesan: String,
+    telpPemesan: String,
+){
+    Text(
+        text = stringResource(R.string.namaOrder)
+    )
+    Text(namaPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    Text(
+        text = stringResource(R.string.alamatOrder)
+    )
+    Text(alamatPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    Text(
+        text = stringResource(R.string.hpOrder),
+    )
+    Text(telpPemesan)
+    Divider()
+
 }
