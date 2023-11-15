18148bba8acb1661f98510ff59169d3f8e9b35f8
raniagisela <raniagslp@gmail.com>
2023-11-16 00:23:02 +0700

    add Mutable List

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,11 +1,28 @@
 package com.example.esjumbo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HalamanForm(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
+
+    var namaTxt by remember{
+        mutableStateOf("")
+    }
+
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+
+    var hpTxt by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,hpTxt)
 
 }
