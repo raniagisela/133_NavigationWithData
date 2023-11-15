aedf4cd25b563b38f449eee00bd73cf0ccd722e0
raniagisela <raniagslp@gmail.com>
2023-11-16 00:26:37 +0700

    add column

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,18 +1,35 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-){
+) {
 
-    var namaTxt by remember{
+    var namaTxt by remember {
         mutableStateOf("")
     }
 
@@ -23,6 +40,26 @@ fun HalamanForm(
     var hpTxt by remember {
         mutableStateOf("")
     }
-    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,hpTxt)
+    var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, hpTxt)
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Masukan Data diri Kamu!", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = namaTxt,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.namaUser)) },
+                onValueChange = { namaTxt = it }
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+        }
+    }
 }
